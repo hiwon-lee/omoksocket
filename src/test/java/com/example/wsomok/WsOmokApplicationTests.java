e6c403b1e15cef2c2c5affe3cf203779297ba244
hiwon-lee <leather2790@naver.com>
2023-11-29 15:10:47 +0900

    Changes

---
+++ src/test/java/com/example/wsomok/WsOmokApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wsomok;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsOmokApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
